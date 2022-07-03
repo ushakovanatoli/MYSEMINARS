facb76eb29ca523d18699afd82ac45802bf24209
anatoli <ushakovanatoli3@mail.ru>
2022-07-03 14:47:15 +0300

    task 1

---
+++ Seminar1/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿int numberA;
+int numberB;
+
+Console.WriteLine("Input numberA: ");
+numberA = Convert.ToInt32( Console.ReadLine());
+
+Console.WriteLine("Input numberB: ");
+numberB = Convert.ToInt32( Console.ReadLine());
+int max = numberA;
+if (numberA == numberB)
+{
+    Console.WriteLine($"numberA{numberA} = numberB{numberB}");
+}
+if (numberB > numberA)
+{
+    Console.WriteLine($" max numderB = {numberB} ");
+}
+else
+{
+    Console.WriteLine($" max numberA = {numberA} ");
+}
