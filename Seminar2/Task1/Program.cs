1b2c94db608040869f0b53467f91627682475711
anatoli <ushakovanatoli3@mail.ru>
2022-07-06 19:29:38 +0300

    fixed task 1 seminar 2

--- Seminar2/Task1/Program.cs
+++ Seminar2/Task1/Program.cs
@@ -1,13 +1,12 @@
 ﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-int ShowSecondDigit(){
-   
-    Console.WriteLine("Input three digit number"  );
+int ShowSecondDigit( ) {
     int number = Convert.ToInt32(Console.ReadLine());
     int num1 = number/10;
     int num2 = num1%10;
     int result =num2;
-    Console.WriteLine(result);
 return  result;
 
 }
- int res = ShowSecondDigit();   
+Console.WriteLine("Input three digit number"  );
+ int res = ShowSecondDigit();
+   Console.WriteLine(res);
